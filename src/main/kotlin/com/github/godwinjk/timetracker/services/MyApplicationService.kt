0f3b565de017657020b4488e964b599b19dc91f6
GitHub Action <action@github.com>
2020-12-16 16:15:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/godwinjk/timetracker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/godwinjk/timetracker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.godwinjk.timetracker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.godwinjk.timetracker.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/godwinjk/timetracker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/godwinjk/timetracker/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.godwinjk.timetracker.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.godwinjk.timetracker.MyBundle
 
 class MyProjectService(project: Project) {
 
